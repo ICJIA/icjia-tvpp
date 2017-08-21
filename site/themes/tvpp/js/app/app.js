31d072ca10e055c7495dfc7fc4d33f16f927c5b8
Chris Schweda <cschweda@gmail.com>
2017-08-21 09:42:26 -0500

    Tweak js

--- site/themes/tvpp/js/app/app.js
+++ site/themes/tvpp/js/app/app.js
@@ -1,12 +1,29 @@
 const App = {
 
-    init: function() {
-
+    highlightSearch: function() {
         $(function() {
             $(".search").focus().select();
         });
+    },
 
+    appDisplay: function() {
         console.log('TVPP Init')
+    },
+
+    init: function() {
+
+        const self = this;
+
+        // Display status
+        this.appDisplay();
+
+        // highlight search term
+
+        $(function() {
+            self.highlightSearch()
+        });
+
+
 
         return
     }
